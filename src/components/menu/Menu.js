1c9043b80d7b0f780db21a64758c18a01d544205
Adebesin-Cell <adebesintolulope80@gmail.com>
2022-05-21 00:02:11 +0100

    updating UI

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,9 +1,35 @@
-import { MenuContainer, MenuNav } from "./Menu.style";
+import {
+  MenuContainer,
+  MenuNav,
+  MenuNavList,
+  MenuNavItem,
+  MenuNavLink,
+  MenuSocials,
+  MenuSocialsHeading,
+} from "./Menu.style";
 
 const Menu = function () {
   return (
     <MenuContainer>
-      <MenuNav>&nbsp;</MenuNav>
+      <MenuNav>
+        <MenuNavList>
+          <MenuNavItem>
+            <MenuNavLink to='/'>Home</MenuNavLink>
+          </MenuNavItem>
+          <MenuNavItem>
+            <MenuNavLink to='/work'>Work</MenuNavLink>
+          </MenuNavItem>
+          <MenuNavItem>
+            <MenuNavLink to='/contact'>Contact</MenuNavLink>
+          </MenuNavItem>
+          <MenuNavItem>
+            <MenuNavLink to='/resume'>Résumé</MenuNavLink>
+          </MenuNavItem>
+        </MenuNavList>
+        <MenuSocials>
+          <MenuSocialsHeading>Connect with me</MenuSocialsHeading>
+        </MenuSocials>
+      </MenuNav>
     </MenuContainer>
   );
 };
--- src/components/menu/Menu.style.js
+++ src/components/menu/Menu.style.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styledComponents from "styled-components";
 
 export const MenuContainer = styledComponents.div`
@@ -17,19 +18,67 @@ export const MenuContainer = styledComponents.div`
 
 export const MenuNav = styledComponents.nav`
     width: 100%;
+    display: flex;
+    flex-direction: column;
 
     @media only screen and (max-width: 990px) {
-        padding: calc(var(--span-2xl) + var(--span-s)) var(--span-l);
-        padding-bottom: calc(var(--span-2xl));
+        padding: calc(var(--span-2xl) + var(--span-2xl)) var(--span-l);
+        padding-bottom: calc(var(--span-2xl) + var(--span-s));
     }
 
     @media only screen and (max-width: 768px) {
-        padding: calc(var(--span-2xl) + var(--span-s)) var(--span-m);
-        padding-bottom: calc(var(--span-2xl));
+        padding: calc(var(--span-2xl) + var(--span-xl)) var(--span-m);
+        padding-bottom: calc(var(--span-2xl) + var(--span-xs));
     }
 
     @media only screen and (max-width: 600px) {
         padding: calc(var(--span-2xl) + var(--span-s)) var(--span-s);
         padding-bottom: calc(var(--span-2xl));
     }
+`;
+
+export const MenuNavList = styledComponents.ul`
+    list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const MenuNavItem = styledComponents.li`
+    margin: calc(var(--span-xs) + 5px) 0;
+    display: flex;
+    justify-content: center;
+`;
+
+export const MenuNavLink = styledComponents(Link)`
+    text-decoration: none;
+    font-family: var(--Inter);
+    font-size: calc(var(--size-m) + 4px);
+    font-weight: var(--weight-sm);
+    line-height: 23px;
+    text-align: center;
+    color: ${({ theme }) => theme.workHeading};
+
+    @media only screen and (max-width: 600px) {
+        font-size: calc(var(--size-m) + 2px);
+        line-height: 21px;
+    }
+`;
+
+export const MenuSocials = styledComponents.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const MenuSocialsHeading = styledComponents.h1`
+    margin: var(--span-s) 0;
+    font-family: var(--Inter);
+    font-size: calc(var(--size-m));
+    font-weight: var(--weight-lg);
+    line-height: 23px;
+    text-align: center;
+    color: ${({ theme }) => theme.active};
+    text-transform: capitalize;
 `;
