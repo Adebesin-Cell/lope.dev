8efdb8cdf6659b2c1de6942a11ba13a081bcf59c
Adebesin Cell <adebesintolulope80@gmail.com>
2023-01-15 07:23:07 +0100

    Added OG image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
-import Work from "../components/work/Work";
-import { HomeWrapper } from "../styles/Home.style";
-import Helmet from "react-helmet";
+import Work from '../components/work/Work';
+import { HomeWrapper } from '../styles/Home.style';
+import Helmet from 'react-helmet';
 
 const Home = function () {
   return (
     <HomeWrapper active='work' title='Home'>
       <Helmet>
+        <link rel='og:image"' href='/images/og-image.png' />
+        <meta property='og:image:width' content='1200' />
+        <meta property='og:image:height' content='630' />
         <title> Lope - Adebesin Tolulope </title>
       </Helmet>
       <Work />
